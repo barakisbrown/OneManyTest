199ac37b08338331cd9d232add8c0152e18b80ba
Matthew Brown <lokislayer@gmail.com>
2018-03-13 12:31:59 -0500

    Copied from local
    This is a copy from my local so that I can load it onto my laptop so I can test it eslewhere when I can get a chance.

---
+++ OneMany/OneMany/Model/Publisher.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using GalaSoft.MvvmLight;
+
+namespace OneMany.Model
+{
+    public class Publisher : ObservableObject
+    {
+
+        // BACKING FIELDS
+        private int _publisherId;
+        private string _name;
+        private bool _imprint;
+
+        public int PublisherId
+        {
+            get => _publisherId;
+            set => Set(ref _publisherId, value);
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+
+        public bool Imprint
+        {
+            get => _imprint;
+            set => Set(ref _imprint, value);
+        }
+
+        // FOREIGN KEY AREA
+        public virtual ICollection<ComicInfo> Comics { get; set; }
+
+        public virtual ICollection<Writer> Writers { get; set; }
+    }
+}
---
+++ OneMany/OneMany/Model/Writer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using GalaSoft.MvvmLight;
+
+namespace OneMany.Model
+{
+    public class Writer : ObservableObject
+    {
+        private int _writerId;
+        private string _name;
+
+        public int WriterId
+        {
+            get => _writerId;
+            set => Set(ref _writerId, value);
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+
+        public int PublisherId { get; set; }
+
+        public virtual ICollection<ComicInfo> Comics { get; set; }
+
+        public virtual Publisher Publisher { get; set; }
+    }
+}
