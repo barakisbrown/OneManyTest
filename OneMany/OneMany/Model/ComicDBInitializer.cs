9575bedb5aaa93fdf801d523327730576af91fc2
Matthew Brown <lokislayer@gmail.com>
2018-03-17 13:36:25 -0500

    Initializer for Seeding DB
    This initializer is only for when I am creating the databasae for the first time.

---
+++ OneMany/OneMany/Model/ComicDBInitializer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace OneMany.Model
+{
+    public class ComicDBInitializer : DropCreateDatabaseAlways<MyDbContext>
+    {
+        protected override void Seed(MyDbContext context)
+        {
+            var P = new Publisher
+            {
+                Name = "Valiant Entertainment",
+                Imprint = false
+            };
+
+            var C = new ComicInfo
+            {
+                Name = "Eternal Warrior",
+                IssueNumber = 1,
+                CoverPrice = 3.99
+            };
+
+            var W = new Writer
+            {
+                Name = "Greg Pag"
+            };
+
+            // PUBLISHER TABLE
+            P.Comics = new List<ComicInfo>();
+            P.Writers = new List<Writer>();
+            // WRITER TABLE
+            W.Comics = new List<ComicInfo>();
+            W.Comics.Add(C);
+            // ADD TO THE DATABASE
+            P.Comics.Add(C);
+            P.Writers.Add(W);
+
+            context.Publisher.Add(P);
+
+            base.Seed(context);
+        }
+    }
+}
