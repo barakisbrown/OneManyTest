383a92c9d6a4e726bf8fbafbf090d0f1becdee98
Matthew Brown <lokislayer@gmail.com>
2018-03-17 15:05:38 -0500

    Add new function InsertComicPart4
    Part 4 shows how to insert the following:
    1. Add writer
    2. Link Writer to existing Publisher
    3. Add Comic
    4. Link Comic to writer/publisher
    5. Save all changes to the backend

--- OneMany/OneMany/Program.cs
+++ OneMany/OneMany/Program.cs
@@ -15,9 +15,10 @@ namespace OneMany
             using (var conn = new MySqlConnection(connString))
             {
                 conn.Open();
-                InsertComics(conn);
+                // InsertComics(conn);
                 // InsertComicsPart2(conn);
-                // InsertComicsPart3(conn);
+                // InsertComicsPart3(conn);  // <= FIXED I THINK
+                InsertComicsPart4(conn);
                 // QueryPublishers(conn);
             }
         }
@@ -73,18 +74,53 @@ namespace OneMany
         {
             using (var db = new MyDbContext(connection, false))
             {
-                var P = db.Publisher.FirstOrDefault(p => p.Name == "Valiant Entertainment");
-                var W = new Writer {Name = "James Asmus"};
+                var P = db.Publisher.FirstOrDefault(p => p.Name == "Valiant Entertainment");                
                 var C = new ComicInfo {Name = "Quantum and Woody", IssueNumber = 8, CoverPrice = 3.99};
                 var C1 = new ComicInfo { Name = "Quantum and Woody", IssueNumber = 9, CoverPrice = 3.99 };
                 var C2 = new ComicInfo { Name = "Quantum and Woody", IssueNumber = 10, CoverPrice = 3.99 };
 
-                P.Writers.Add(W);
-                // P.Comics.Add(C);
-                // P.Comics.Add(C1);
-                // P.Comics.Add(C2);
+                /*
+                    var W = new Writer {Name = "James Asmus"};
+                    // LET SEE IF I CAN ADD JUST A NEW WRITER AND LINK IT TO ID#1[Valiant Entertainment]
+                    W.Publisher = P;
+                    db.Writer.Add(W);
+                */
+                // ^THE ABOVE WORKED^
+                var W = db.Writer.FirstOrDefault(w => w.Name == "James Asmus");
+                // COMIC 1
+                C.Publisher = P;
+                C.Writer = W;
+                // COMIC 2
+                C1.Publisher = P;
+                C1.Writer = W;
+                // COMIC 3
+                C2.Publisher = P;
+                C2.Writer = W;
+                // ADD COMICS TO THE TABLES
+                db.Comics.Add(C);
+                db.Comics.Add(C1);
+                db.Comics.Add(C2);
+                db.SaveChanges();
+            }
+        }
 
-                db.Publisher.Add(P);
+        public static void InsertComicsPart4(MySqlConnection connection)
+        {
+            using (var db = new MyDbContext(connection,false))
+            {
+                // LETS LINK THE NEW WRITER TO THE PUBLISHER [Paul Cornel to Marvel]
+                var w = new Writer() {Name = "Paul Cornell"};
+                var P = db.Publisher.FirstOrDefault(p => p.Name == "Marvel");
+                w.Publisher = P;
+                db.Writer.Add(w);
+                db.SaveChanges();
+                // AT THIS POINT THE WRITER SHOULD BE SAVED IN THE TABLE AND LINKED TO PUBLISHER
+                var W = db.Writer.FirstOrDefault(w1 => w1.Name == "Paul Cornell");
+                var C = new ComicInfo() {Name = "Wolverine", IssueNumber = 1, CoverPrice = 3.99};
+                C.Writer = W;
+                C.Publisher = P;
+                // ADD COMIC TO THE TABLE AND THEN SUBMIT TO DATABASE
+                db.Comics.Add(C);
                 db.SaveChanges();
             }
         }
